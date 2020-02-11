99b2a72752040f8dfb9a43c9dbd40caac092d78d
makobot-prod <45239258+makobot-prod@users.noreply.github.com>
2020-02-11 19:49:20 -0300

    Nueva version de stretch

--- src/efectos.c
+++ src/efectos.c
@@ -64,7 +64,7 @@ float* stretch(float* audio, unsigned int size, float f, unsigned int window_siz
 		hanning[i] *= hanning[i];
 	}
 
-	for (unsigned int i = 0; i < size - window_size - hop; i+=f*hop)
+	for (unsigned int i = 0; i < size - window_size - hop; i+=hop)
 	{
 		float a1[window_size];
 		float a2[window_size];
@@ -77,8 +77,8 @@ float* stretch(float* audio, unsigned int size, float f, unsigned int window_siz
 		complejo s1[window_size];
 		complejo s2[window_size];
 
-		ditfft2(a1,window_size,s1);
-		ditfft2(a2,window_size,s2);
+		ditfft2_asm(a1,window_size,s1);
+		ditfft2_asm(a2,window_size,s2);
 
 		for (int j = 0; j < window_size; ++j)
 		{
@@ -86,15 +86,19 @@ float* stretch(float* audio, unsigned int size, float f, unsigned int window_siz
 			float normas2 = sqrt(s2[j].real*s2[j].real + s2[j].imaginaria*s2[j].imaginaria);
 
 			complejo frac;
-			
 			frac.real = (s1[j].real*s2[j].real + s1[j].imaginaria*s2[j].imaginaria);
-			frac.imaginaria = (s1[j].real*s2[j].imaginaria - s1[j].imaginaria*s2[j].real);	
+			frac.imaginaria = (s1[j].real*s2[j].imaginaria - s1[j].imaginaria*s2[j].real);
 
 			float v_angular = (float) atan2((double) frac.imaginaria, (double) frac.real);
 
-			phase[j] += v_angular;
-			if (phase[j] > M_PI) {phase[j] -= 2*M_PI;}
-			else if(phase[j] < - M_PI) {phase[j] += 2*M_PI;}
+			float omega = hop*j*2*M_PI/window_size;
+
+			float delta_phi = v_angular - omega;
+			delta_phi = delta_phi - ((int) (delta_phi/(2*M_PI)))*2*M_PI;
+			delta_phi += omega;
+
+			phase[j] += delta_phi/f;
+			phase[j] = phase[j] - ((int) (phase[j]/(2*M_PI)))*2*M_PI;
 
 			complejo rephased;
 			rephased.real = cos(phase[j])*normas2;
@@ -107,7 +111,7 @@ float* stretch(float* audio, unsigned int size, float f, unsigned int window_siz
 
 		}
 
-		iditfft2(s2,window_size,s1);
+		iditfft2_asm(s2,window_size,s1);
 
 		for (int j = 0; j < window_size; ++j)
 		{
@@ -148,25 +152,10 @@ void efecto_repitch(float* audio, unsigned int size, float f) {
 
 	float resample_coef = f;
 
-	temp = stretch(audio, audio_in_info.frames, 1./resample_coef, 512,512/2);
+	temp = stretch(audio, audio_in_info.frames, 1./resample_coef, 2048,2048/16);
 	output = resample(temp, (unsigned int)  audio_in_info.frames*resample_coef,  resample_coef);
 
 	save_wav_len("repitch.wav",output,audio_in_info.frames);
-
-	free(temp);
-	free(output);
-}
-
-void efecto_repitch_asm(float* audio, unsigned int size, float f) {
-	float*  temp;  
-	float* output;
-
-	float resample_coef = f;
-
-	temp = stretch_asm(audio, audio_in_info.frames, 1./resample_coef, 512,512/2);
-	output = resample_asm(temp, (unsigned int)  audio_in_info.frames*resample_coef,  resample_coef);
-
-	save_wav_len("repitch_asm.wav",output,audio_in_info.frames);
 
 	free(temp);
 	free(output);
@@ -177,24 +166,32 @@ void efecto_repitch_asm(float* audio, unsigned int size, float f) {
 void efecto_vocoder(float* modulator, float* carrier, unsigned int window_size) {
 	unsigned int size = audio_in_info.frames;
 	float* output = calloc(size,sizeof(float));
+	float * ref = output;
+	vocoder_asm(modulator,carrier,window_size,output,size);
+	//  para bajar el volumen un poco
+	for (int i = 0; i < size; ++i)
+	{
+		output[i] /= 50.0;
+	}
+	save_wav("vocoder.wav",output);
+	free(ref);
+}
+
+
+void vocoder(float* modulator, float* carrier, unsigned int window_size, float* output, unsigned int size) {
 	float hanning[window_size];
 
-	for (int i = 0; i < window_size; ++i)
-	{
-		hanning[i] = (float) sin((M_PI*i)/(window_size-1));
-		hanning[i] *= hanning[i];
-	}
-
+	precalcular_hanning(hanning, window_size);
 	complejo F1[window_size];
 	complejo F2[window_size];
 
-	for (int i = 0; i < size - window_size-1; i += window_size/2)
+	for (int i = 0; i < size - window_size+1; i += window_size/2)
 	{
 		ditfft2_asm(&modulator[i],window_size,F1);
 		ditfft2_asm(&carrier[i],window_size,F2);
 
 		for(int j = 0; j < window_size; j++) {
-			float modulo = (F1[j].real*F1[j].real + F1[j].imaginaria*F1[j].imaginaria); 
+			float modulo = sqrt(F1[j].real*F1[j].real + F1[j].imaginaria*F1[j].imaginaria); 
 			F2[j].real *= modulo;
 			F2[j].imaginaria *= modulo;
 		}
@@ -202,10 +199,15 @@ void efecto_vocoder(float* modulator, float* carrier, unsigned int window_size) 
 		iditfft2_asm(F2,window_size,F1);
 
 		for(int j = 0; j < window_size; j++) {
-			output[i+j] += F1[j].real*hanning[j]/50.0;
+			output[i+j] += F1[j].real*hanning[j];
 		}
-	}
+	}	
+}
 
-	save_wav("vocoder.wav",output);
-	free(output);
+void precalcular_hanning(float* hanning, unsigned int window_size) {
+	for (int i = 0; i < window_size; ++i)
+	{
+		hanning[i] = (float) sin((M_PI*i)/(window_size-1));
+		hanning[i] *= hanning[i];
+	}
 }
